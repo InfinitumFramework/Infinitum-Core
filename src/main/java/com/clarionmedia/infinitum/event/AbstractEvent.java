94312fbbd7222a0bf971d1184f373e8df5bf8a0b
Tyler Treat <tyler.treat@webfilings.com>
2013-03-14 13:13:35 -0500

    Change AbstractEvent payload to a map.

--- src/main/java/com/clarionmedia/infinitum/event/AbstractEvent.java
+++ src/main/java/com/clarionmedia/infinitum/event/AbstractEvent.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 Clarion Media, LLC
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,52 +19,55 @@ package com.clarionmedia.infinitum.event;
 import android.app.Activity;
 import com.clarionmedia.infinitum.context.InfinitumContext;
 
+import java.util.Map;
+
 /**
  * <p>
  * Abstract representation of an event that has been triggered by the framework
  * or on behalf of the framework. A {@code FrameworkEvent} may be fired off from
  * an {@link Activity} lifecycle hook, such as {@code onCreate}, from the
- * invocation of a method annotated with {@link InfinitumEvent}, or manually by
+ * invocation of a method annotated with {@link com.clarionmedia.infinitum.event.annotation.Event}, or manually by
  * calling {@link InfinitumContext#publishEvent(AbstractEvent)}.
  * </p>
- * 
+ *
  * @author Tyler Treat
- * @version 1.0 01/18/13
+ * @version 1.0.4 03/13/13
+ * @see com.clarionmedia.infinitum.event.annotation.Event
  * @since 1.0
- * @see InfinitumEvent
  */
 public abstract class AbstractEvent {
 
-	protected Object mPublisher;
-	protected String mName;
-	protected Object mPayload;
+    protected Object mPublisher;
+    protected String mName;
+    protected Map<String, Object> mPayload;
 
-	/**
-	 * Creates a new {@code FrameworkEvent} instance.
-	 * 
-	 * @param name
-	 *            the name of the event
-	 * @param publisher
-	 *            the publisher of the event
-	 * @param payload
-	 *            the payload, if any, of the event
-	 */
-	public AbstractEvent(String name, Object publisher, Object payload) {
-		mName = name;
-		mPublisher = publisher;
-		mPayload = payload;
-	}
+    /**
+     * Creates a new {@code FrameworkEvent} instance.
+     *
+     * @param name      the name of the event
+     * @param publisher the publisher of the event
+     * @param payload   the payload, if any, of the event
+     */
+    public AbstractEvent(String name, Object publisher, Map<String, Object> payload) {
+        mName = name;
+        mPublisher = publisher;
+        mPayload = payload;
+    }
 
-	public Object getPublisher() {
-		return mPublisher;
-	}
+    public Object getPublisher() {
+        return mPublisher;
+    }
 
-	public String getName() {
-		return mName;
-	}
+    public String getName() {
+        return mName;
+    }
 
-	public Object getPayload() {
-		return mPayload;
-	}
+    public boolean containsPayloadValue(String name) {
+        return mPayload.containsKey(name);
+    }
+
+    public Object getPayloadValue(String name) {
+        return mPayload.get(name);
+    }
 
 }
